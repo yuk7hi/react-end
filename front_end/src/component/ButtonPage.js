1767808566a66c808718c3bcc381f773d549b048
Dark_Trigger <withanageyasiru@gmail.com>
2019-01-31 00:20:09 +0530

    login

---
+++ front_end/src/component/ButtonPage.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import { MDBBtn } from "mdbreact";
+
+const ButtonPage = () => {
+    return (
+        <Fragment>
+            <MDBBtn color="primary">Primary</MDBBtn>
+            <MDBBtn>Default</MDBBtn>
+            <MDBBtn color="secondary">Secondary</MDBBtn>
+            <MDBBtn color="success">Success</MDBBtn>
+            <MDBBtn color="info">Info</MDBBtn>
+            <MDBBtn color="warning">Warning</MDBBtn>
+            <MDBBtn color="danger">Danger</MDBBtn>
+        </Fragment>
+    );
+}
+
+export default ButtonPage
