fb84d8d42646ba3b83ac43a5d2082390a33e8ceb
yuk7hi <yukthi@hotmail.com>
2019-02-03 21:30:45 +0530

    edit index.js with Router

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,9 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
-    <App />,
+    <Router>
+        <App />
+    </Router>,
     document.getElementById('root')
 );
 
