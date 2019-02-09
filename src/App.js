5e1a35edf93e54a1badbdea89a94d0f072526c63
yuk7hi <yukthi@hotmail.com>
2019-02-09 14:25:14 +0530

    create home-route branch for issue #7

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
         this.state = {
             isAuthenticated: false,
             isAuthenticating: true,
-            accessToken: "",
+            accessToken: "0",
             levelOfAuthentication: 0
         };
     }
