3d923b53a22c30f9580c2153ed5bb2e5fe339a56
yuk7hi <yukthi@hotmail.com>
2019-02-11 18:46:57 +0530

    minor updates to 404 page

--- src/containers/UnknownPage.js
+++ src/containers/UnknownPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 let UnknownPage = () => (
-    <div className="UnknownPage">
-        <h1>ERROR 404</h1>
-        <h2>Sorry, Page you're looking for is not here!</h2>
+    <div className="UnknownPage flex-center text-center">
+        <div>
+            <h1>ERROR 404</h1>
+            <h2>Sorry, Page you're looking for is not here!</h2>
+        </div>
     </div>
 );
 
